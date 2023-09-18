c23bbc41c0574c4dc666dc7b2ff09478a82be7b4
Max <markushitstep@gmail.com>
2023-09-18 20:48:13 +0300

    refactor/add the ability to simulate a server error

--- src/assets/ModalModule.jsx
+++ src/assets/ModalModule.jsx
@@ -1,10 +1,29 @@
-import { modalMapping } from "../components/Modal/Modal";
+import ModalError from "../components/Modal/ModalError/ModalError";
+import ModalInfo from "../components/Modal/ModalInfo/ModalInfo";
+import ModalWarning from "../components/Modal/ModalWarning/ModalWarning";
 
 class ModalModule {
-  component = (<></>);
+  #component = null;
+  #modalMapping = {};
+  #props;
   setType(type) {
-    this.component = modalMapping[type];
+    this.#component = this.#createModalMapping(type);
+  }
+  setProps(props) {
+    this.#props = props;
+  }
+  #createModalMapping(type) {
+    this.#modalMapping = {
+      Info: ModalInfo,
+      Warning: ModalWarning,
+      Error: ModalError,
+    };
+    return this.#modalMapping[type];
+  }
+  getComponent() {
+    const Component = this.#component;
+    if (!this.#props || !this.#component) return null;
+    return <Component {...this.#props} />;
   }
 }
-const modalModule = new ModalModule();
-export default modalModule;
+export default ModalModule;
